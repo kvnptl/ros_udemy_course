7dec1107c0299a6ec64be9a4dab62ae732e797b7
kvnptl <kevinpatel4400@gmail.com>
2022-02-10 14:31:11 +0100

    new: create basic cpp node

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ros_udemy_course
+# Overview
 ROS For Beginners (ROS Noetic, Ubuntu 20.04) Udemy course created by Edouard Renard
+
+Course link: https://www.udemy.com/course/ros-essentials/
+
+### Notes:
+1. After cloning the repo, delete build, devel, and logs folders
+2. Run `catkin build` command
+
--- catkin_ws/src/my_robot_tutorials/scripts/my_first_node.py
+++ catkin_ws/src/my_robot_tutorials/scripts/my_first_node.py
@@ -1,13 +1,23 @@
 #i/usr/bin/env python3
+#The first line is called shebang, or hash-bang line.
+# What is the shebang? 
+# The shebang is a special character sequence in a script file that specifies 
+# which program should be called to run the script.
+# source: https://www.a2hosting.com/kb/developer-corner/linux/using-the-shebang
 
 import rospy
 
-
 if __name__ == '__main__':
     rospy.init_node("my_first_python_node")
-
     rospy.loginfo("This node has been started")
 
-    rospy.sleep(1)
+    #set 10Hz frequency
+    rate = rospy.Rate(10)
 
-    rospy.loginfo("Exit now")
+    #wait untill node killed by the user
+    while not rospy.is_shutdown():
+        rospy.loginfo("Hello")
+        
+        #wait for 0.1 sec, that means send run this loop 10 times a second
+        #This way it is 10 Hz (10 msgs under 1 sec)
+        rate.sleep()
